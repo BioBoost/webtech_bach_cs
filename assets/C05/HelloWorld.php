15d29108769957e4f847cdc024d561b27fd71783
Nico De Witte <nico.dewitte@khbo.be>
2013-10-12 14:59:08 +0200

    Added h1 with hello world

---
+++ assets/C05/HelloWorld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Exciting world of PHP</title>
+    </head>
+    <body>
+        <h1>Hello world</h1>
+        <?php
+            // Output paragraph
+            echo "<p>Welcome to the exciting world of PHP.</p>";
+        ?>
+    </body>
+</html>
